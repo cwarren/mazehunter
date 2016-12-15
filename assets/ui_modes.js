a30367a22f2b7e387332b38ade19fb52143bd5a3
Chris <csw11235@gmail.com>
2016-12-15 20:58:06 +0000

    basic entities implemented with mixins (including a corporeal walker); avatar is an entity; can move the avatar around the maze and not walk through walls

--- assets/ui_modes.js
+++ assets/ui_modes.js
@@ -91,6 +91,38 @@ Game.UIMode.gamePersistence = {
       Game.Message.send('Sorry, no local data storage is available for this browser');
   		return false;
   	}
+  },
+  BASE_toJSON: function(state_hash_name) {
+    var state = this.attr;
+    if (state_hash_name) {
+      state = this[state_hash_name];
+    }
+    var json = {};
+    for (var at in state) {
+      if (state.hasOwnProperty(at)) {
+        if (state[at] instanceof Object && 'toJSON' in state[at]) {
+          json[at] = state[at].toJSON();
+        } else {
+          json[at] = state[at];
+        }
+      }
+    }
+    return json;
+  },
+  BASE_fromJSON: function (json,state_hash_name) {
+    var using_state_hash = 'attr';
+    if (state_hash_name) {
+      using_state_hash = state_hash_name;
+    }
+    for (var at in this[using_state_hash]) {
+      if (this[using_state_hash].hasOwnProperty(at)) {
+        if (this[using_state_hash][at] instanceof Object && 'fromJSON' in this[using_state_hash][at]) {
+          this[using_state_hash][at].fromJSON(json[at]);
+        } else {
+          this[using_state_hash][at] = json[at];
+        }
+      }
+    }
   }
 };
 
@@ -103,8 +135,7 @@ Game.UIMode.gamePlay = {
     _mapHeight: 200,
     _cameraX: 100,
     _cameraY: 100,
-    _avatarX: 100,
-    _avatarY: 100
+    _avatar: null
   },
   JSON_KEY: 'uiMode_gamePlay',
   enter: function () {
@@ -126,19 +157,19 @@ Game.UIMode.gamePlay = {
     this.renderAvatar(display);
   },
   renderAvatar: function (display) {
-    Game.Symbol.AVATAR.draw(display,this.attr._avatarX-this.attr._cameraX+display._options.width/2,
-                                    this.attr._avatarY-this.attr._cameraY+display._options.height/2);
+    Game.Symbol.AVATAR.draw(display,this.attr._avatar.getX()-this.attr._cameraX+display._options.width/2,
+                                    this.attr._avatar.getY()-this.attr._cameraY+display._options.height/2);
   },
   renderAvatarInfo: function (display) {
     var fg = Game.UIMode.DEFAULT_COLOR_FG;
     var bg = Game.UIMode.DEFAULT_COLOR_BG;
-    display.drawText(1,2,"avatar x: "+this.attr._avatarX,fg,bg); // DEV
-    display.drawText(1,3,"avatar y: "+this.attr._avatarY,fg,bg); // DEV
+    display.drawText(1,2,"avatar x: "+this.attr._avatar.getX(),fg,bg); // DEV
+    display.drawText(1,3,"avatar y: "+this.attr._avatar.getY(),fg,bg); // DEV
   },
   moveAvatar: function (dx,dy) {
-    this.attr._avatarX = Math.min(Math.max(0,this.attr._avatarX + dx),this.attr._mapWidth);
-    this.attr._avatarY = Math.min(Math.max(0,this.attr._avatarY + dy),this.attr._mapHeight);
-    this.setCameraToAvatar();
+    if (this.attr._avatar.tryWalk(this.attr._map,dx,dy)) {
+      this.setCameraToAvatar();
+    }
   },
   moveCamera: function (dx,dy) {
     this.setCamera(this.attr._cameraX + dx,this.attr._cameraY + dy);
@@ -146,9 +177,10 @@ Game.UIMode.gamePlay = {
   setCamera: function (sx,sy) {
     this.attr._cameraX = Math.min(Math.max(0,sx),this.attr._mapWidth);
     this.attr._cameraY = Math.min(Math.max(0,sy),this.attr._mapHeight);
+    Game.refresh();
   },
   setCameraToAvatar: function () {
-    this.setCamera(this.attr._avatarX,this.attr._avatarY);
+    this.setCamera(this.attr._avatar.getX(),this.attr._avatar.getY());
   },
   handleInput: function (inputType,inputData) {
     var pressedKey = String.fromCharCode(inputData.charCode);
@@ -181,7 +213,6 @@ Game.UIMode.gamePlay = {
       } else if (pressedKey == '9') {
         this.moveAvatar(1,-1);
       }
-      Game.refresh();
     }
     else if (inputType == 'keydown') {
       // console.log('gameStart inputType:');
@@ -219,26 +250,22 @@ Game.UIMode.gamePlay = {
     // create map from the tiles
     this.attr._map =  new Game.Map(mapTiles);
 
+    this.attr._avatar = new Game.Entity(Game.EntityTemplates.Avatar);
+
     // restore anything else if the data is available
     if (restorationData !== undefined && restorationData.hasOwnProperty(Game.UIMode.gamePlay.JSON_KEY)) {
       this.fromJSON(restorationData[Game.UIMode.gamePlay.JSON_KEY]);
+    } else {
+      this.attr._avatar.setPos(this.attr._map.getRandomWalkableLocation());
     }
+
+    this.setCameraToAvatar();
   },
   toJSON: function() {
-    var json = {};
-    for (var at in this.attr) {
-      if (this.attr.hasOwnProperty(at) && at!='_map') {
-        json[at] = this.attr[at];
-      }
-    }
-    return json;
+    return Game.UIMode.gamePersistence.BASE_toJSON.call(this);
   },
   fromJSON: function (json) {
-    for (var at in this.attr) {
-      if (this.attr.hasOwnProperty(at) && at!='_map') {
-        this.attr[at] = json[at];
-      }
-    }
+    Game.UIMode.gamePersistence.BASE_fromJSON.call(this,json);
   }
 };
 
