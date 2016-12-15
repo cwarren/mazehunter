7f2538d2b156cc80b3b8554d2b0c12f4325627dc
Chris <csw11235@gmail.com>
2016-12-15 20:00:05 +0000

    simplest game

--- assets/game.js
+++ assets/game.js
@@ -1,7 +1,7 @@
 console.log("hello?");
 
 window.onload = function() {
-    console.log("starting WSRL - window loaded");
+    console.log("starting MH - window loaded");
     // Check if rot.js can work on this browser
     if (!ROT.isSupported()) {
         alert("The rot.js library isn't supported by your browser.");
@@ -9,43 +9,113 @@ window.onload = function() {
         // Initialize the game
         Game.init();
 
-        // Add the containers to our HTML page
-        document.getElementById('mh-main-display').appendChild(Game.getDisplay('main').getContainer());
+        document.getElementById('mh-avatar-display').appendChild(   Game.getDisplay('avatar').getContainer());
+        document.getElementById('mh-main-display').appendChild(   Game.getDisplay('main').getContainer());
+        document.getElementById('mh-message-display').appendChild(   Game.getDisplay('message').getContainer());
+
+        var bindEventToScreen = function(eventType) {
+            window.addEventListener(eventType, function(evt) {
+              Game.eventHandler(eventType, evt);
+            });
+        };
+        // Bind keyboard input events
+        bindEventToScreen('keypress');
+        bindEventToScreen('keydown');
+//        bindEventToScreen('keyup');
+
+        Game.switchUiMode(Game.UIMode.gameStart);
     }
 };
 
 var Game = {
 
-  display: {
-    SPACING: 1.1,
+  _DISPLAY_SPACING: 1.1,
+  _display: {
     main: {
       w: 80,
       h: 24,
       o: null
+    },
+    avatar: {
+      w: 20,
+      h: 24,
+      o: null
+    },
+    message: {
+      w: 100,
+      h: 6,
+      o: null
     }
   },
+  _curUiMode: null,
 
-  init: function() {
-    this._randomSeed = 5 + Math.floor(Math.random()*100000);
-    //this._randomSeed = 76250;
-    console.log("using random seed "+this._randomSeed);
-    ROT.RNG.setSeed(this._randomSeed);
+    init: function() {
+      this._randomSeed = 5 + Math.floor(Math.random()*100000);
+      //this._randomSeed = 76250;
+      console.log("using random seed "+this._randomSeed);
+      ROT.RNG.setSeed(this._randomSeed);
 
-    this.display.main.o = new ROT.Display({width: this.display.main.w, height: this.display.main.h, spacing: Game.display.SPACING});
-    this.renderMain();
-  },
+      for (var display_key in this._display) {
+        if (this._display.hasOwnProperty(display_key)) {
+          this._display[display_key].o = new ROT.Display({width: this._display[display_key].w, height: this._display[display_key].h, spacing: Game._DISPLAY_SPACING});
+        }
+      }
+      this.renderDisplayAll();
+    },
 
-  getDisplay: function (displayId) {
-    if (this.display.hasOwnProperty(displayId)) {
-      return this.display[displayId].o;
+    getDisplay: function (displayId) {
+      if (this._display.hasOwnProperty(displayId)) {
+        return this._display[displayId].o;
+      }
+      return null;
+    },
+
+    refresh: function () {
+      this.renderDisplayAll();
+    },
+    renderDisplayAll: function() {
+      this.renderDisplayAvatar();
+      this.renderDisplayMain();
+      this.renderDisplayMessage();
+    },
+    renderDisplayAvatar: function() {
+      this._display.avatar.o.clear();
+      if (this._curUiMode === null) {
+        return;
+      }
+      if (this._curUiMode.hasOwnProperty('renderAvatar')) {
+        this._curUiMode.renderAvatar(this._display.avatar.o);
+      }
+    },
+    renderDisplayMain: function() {
+      this._display.main.o.clear();
+      if (this._curUiMode === null) {
+        return;
+      }
+      if (this._curUiMode.hasOwnProperty('render')) {
+        this._curUiMode.render(this._display.main.o);
+      }
+    },
+    renderDisplayMessage: function() {
+      Game.Message.render(this._display.message.o);
+    },
+
+    eventHandler: function (eventType, evt) {
+      // When an event is received have the current ui handle it
+      if (this._curUiMode !== null) {
+          this._curUiMode.handleInput(eventType, evt);
+          Game.refresh();
+      }
+    },
+
+    switchUiMode: function (newUiMode) {
+      if (this._curUiMode !== null) {
+        this._curUiMode.exit();
+      }
+      this._curUiMode = newUiMode;
+      if (this._curUiMode !== null) {
+        this._curUiMode.enter();
+      }
+      this.renderDisplayAll();
     }
-    return null;
-  },
-
-  renderMain: function() {
-    var d = this.display.main.o;
-    for (var i = 0; i < 10; i++) {
-      d.drawText(5,i+5,"this is the world of maze hunter!");
-    }
-  }
-};
+  };
---
+++ assets/ui_modes.js
@@ -0,0 +1,105 @@
+Game.UIMode = {};
+Game.UIMode.DEFAULT_COLOR_FG = '#fff';
+Game.UIMode.DEFAULT_COLOR_BG = '#000';
+Game.UIMode.DEFAULT_COLOR_STR = '%c{'+Game.UIMode.DEFAULT_COLOR_FG+'}%b{'+Game.UIMode.DEFAULT_COLOR_BG+'}';
+
+Game.UIMode.gameStart = {
+  enter: function () {
+    console.log('game starting');
+    Game.Message.send("Welcome to MH");
+  },
+  exit: function () {
+  },
+  render: function (display) {
+    var fg = Game.UIMode.DEFAULT_COLOR_FG;
+    var bg = Game.UIMode.DEFAULT_COLOR_BG;
+    display.drawText(1,1,"game start",fg,bg);
+    display.drawText(1,3,"press any key to play",fg,bg);
+  },
+  handleInput: function (inputType,inputData) {
+  //  console.log('gameStart inputType:');
+  //  console.dir(inputType);
+  //  console.log('gameStart inputData:');
+  //  console.dir(inputData);
+    if (inputData.charCode !== 0) { // ignore the various modding keys - control, shift, etc.
+      Game.switchUiMode(Game.UIMode.gamePlay);
+    }
+  }
+};
+
+Game.UIMode.gamePlay = {
+  enter: function () {
+    console.log('game playing');
+    Game.Message.clear();
+  },
+  exit: function () {
+  },
+  render: function (display) {
+    var fg = Game.UIMode.DEFAULT_COLOR_FG;
+    var bg = Game.UIMode.DEFAULT_COLOR_BG;
+    display.drawText(1,1,"game play",fg,bg);
+    display.drawText(1,3,"press [Enter] to win",fg,bg);
+    display.drawText(1,4,"press [Esc] to lose",fg,bg);
+  },
+  handleInput: function (inputType,inputData) {
+    console.log('gameStart inputType:');
+    console.dir(inputType);
+    console.log('gameStart inputData:');
+    console.dir(inputData);
+    Game.Message.send("you pressed the '"+String.fromCharCode(inputData.charCode)+"' key");
+    if (inputType == 'keypress') {
+      var inputKey = inputData.keyIdentifier;
+      if (inputKey === undefined) {
+        inputKey = inputData.key;
+      }
+      if (inputKey == 'Enter') {
+        Game.switchUiMode(Game.UIMode.gameWin);
+      }
+    }
+    else if (inputType == 'keydown') {
+      if (inputData.keyCode == 27) {
+        Game.switchUiMode(Game.UIMode.gameLose);
+      }
+    }
+  }
+};
+
+Game.UIMode.gameWin = {
+  enter: function () {
+    console.log('game winning');
+  },
+  exit: function () {
+  },
+  render: function (display) {
+    var fg = Game.UIMode.DEFAULT_COLOR_FG;
+    var bg = Game.UIMode.DEFAULT_COLOR_BG;
+    display.drawText(1,1,"You WON!!!!",fg,bg);
+  },
+  handleInput: function (inputType,inputData) {
+    // console.log('gameStart inputType:');
+    // console.dir(inputType);
+    // console.log('gameStart inputData:');
+    // console.dir(inputData);
+    Game.Message.clear();
+  }
+};
+
+Game.UIMode.gameLose = {
+  enter: function () {
+    console.log('game losing');
+  },
+  exit: function () {
+  },
+  render: function (display) {
+    var fg = Game.UIMode.DEFAULT_COLOR_FG;
+    var bg = Game.UIMode.DEFAULT_COLOR_BG;
+    display.drawText(1,1,"You lost :(",fg,bg);
+  },
+  handleInput: function (inputType,inputData) {
+    // console.log('gameStart inputType:');
+    // console.dir(inputType);
+    // console.log('gameStart inputData:');
+    // console.dir(inputData);
+    Game.Message.clear();
+  }
+};
